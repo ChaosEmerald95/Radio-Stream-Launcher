5cec74b40dd5cde24207e363dbd99c7ae692e008
ChaosEmerald95 <p.loessner95@gmail.com>
2015-10-28 19:26:50 +0100

    Add InputBoxSharp-Code to Project
    InputBoxSharp-Code now added to Project
    .gitignore Updated

---
+++ InputBoxSharp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InputBoxSharp
+{
+    partial class InputBoxSharp : Form
+    {
+        string _defaultresponse = "";
+        string _response;
+
+        /// <summary>
+        /// Erstellt eine neue Instanz von InputBoxSharp
+        /// </summary>
+        public InputBoxSharp(string prompt, string title, string defaultresponse)
+        {
+            InitializeComponent();
+            txtinput.Text = "";
+            _response = _defaultresponse;
+            description = prompt;
+            this.title = title;
+            this.defaultresponse = defaultresponse;
+        }
+
+        /// <summary>
+        /// Erstellt eine neue Instanz von InputBoxSharp
+        /// </summary>
+        public InputBoxSharp(string prompt, string title, string defaultresponse, bool pwchar)
+        {
+            InitializeComponent();
+            txtinput.Text = "";
+            _response = _defaultresponse;
+            description = prompt;
+            this.title = title;
+            this.defaultresponse = defaultresponse;
+            //Passwordchar
+            if (pwchar == true) txtinput.PasswordChar = '*';
+            if (pwchar == false) txtinput.PasswordChar = ' ';
+        }
+
+        /// <summary>
+        /// Erstellt eine neue Instanz von InputBoxSharp
+        /// </summary>
+        public InputBoxSharp(string prompt, string title, string defaultresponse, bool pwchar, int XPos, int YPos)
+        {
+            InitializeComponent();
+            txtinput.Text = "";
+            _response = _defaultresponse;
+            description = prompt;
+            this.title = title;
+            this.defaultresponse = defaultresponse;
+            //Passwordchar
+            if (pwchar == true) txtinput.PasswordChar = '*';
+            if (pwchar == false) txtinput.PasswordChar = ' ';
+            this.Location = new Point(XPos, YPos); 
+        }
+
+        /// <summary>
+        /// Legt den Title des Fensters fest
+        /// </summary>
+        public string title
+        {
+            set 
+            {
+                if (value == "")
+                {
+                    this.Text = System.Reflection.Assembly.GetExecutingAssembly().FullName;
+                }
+                else
+                {
+                    this.Text = value;
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Legt die Beschreibung der InputBox fest
+        /// </summary>
+        public string description
+        {
+            set 
+            {
+                lbdescription.Text = "";
+                string[] lines = value.Split(System.Text.Encoding.ASCII.GetChars(new byte[] {13,10}));
+                int i;
+                for (i = 0; i < lines.Length; i++)
+                {
+                    lbdescription.Text = lbdescription.Text + lines[i] + Environment.NewLine;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Legt den Standardrückgabewert fest
+        /// </summary>
+        public string defaultresponse
+        {
+            set { txtinput.Text = value; }
+        }
+
+        private void btnabort_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+        
+        /// <summary>
+        /// Gibt die Antwort aus der InputBox zurück
+        /// </summary>
+        /// <returns></returns>
+        public string Response()
+        {
+            return _response; 
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            _response = txtinput.Text;
+            this.Close(); 
+        }
+    }
+}
---
+++ InputBoxSharp/InputBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InputBoxSharp
+{
+    public static class InputBoxSharpClass
+    {
+        /// <summary>
+        /// Zeigt eine Eingabeaufforderung in einem Dialogfeld an, wartet auf die Eingabe eines Textes oder auf das Klicken auf eine Schaltfläche durch den Benutzer und gibt eine Zeichenfolge zurück, die den Inhalt des Textfelds angibt.
+        /// </summary>
+        /// <param name="Prompt">Als Meldung im Dialogfeld angezeigter String-Ausdruck. Die maximale Länge von Prompt beträgt ungefähr 1024 Zeichen, abhängig von der Breite der verwendeten Zeichen. Wenn Prompt aus mehreren Zeilen besteht, können Sie die Zeilen trennen, indem Sie jeweils ein Wagenrücklaufzeichen(Chr(13)), ein Zeilenvorschubzeichen(Chr(10)) oder eine Kombination aus Wagenrücklauf und Zeilenvorschub (Chr(13) & Chr(10)) zwischen jeder Zeile einfügen.</param>
+        /// <returns></returns>
+        public static string InputBox(string Prompt)
+        {
+            InputBoxSharp frm = new InputBoxSharp(Prompt, "", "");
+            frm.ShowDialog();
+            return frm.Response(); 
+        }
+
+        /// <summary>
+        /// Zeigt eine Eingabeaufforderung in einem Dialogfeld an, wartet auf die Eingabe eines Textes oder auf das Klicken auf eine Schaltfläche durch den Benutzer und gibt eine Zeichenfolge zurück, die den Inhalt des Textfelds angibt.
+        /// </summary>
+        /// <param name="Prompt">Als Meldung im Dialogfeld angezeigter String-Ausdruck. Die maximale Länge von Prompt beträgt ungefähr 1024 Zeichen, abhängig von der Breite der verwendeten Zeichen. Wenn Prompt aus mehreren Zeilen besteht, können Sie die Zeilen trennen, indem Sie jeweils ein Wagenrücklaufzeichen(Chr(13)), ein Zeilenvorschubzeichen(Chr(10)) oder eine Kombination aus Wagenrücklauf und Zeilenvorschub (Chr(13) & Chr(10)) zwischen jeder Zeile einfügen.</param>
+        /// <param name="Title">Auf der Titelleiste des Dialogfelds angezeigter String-Ausdruck. Wenn Die Title nicht angeben, wird der Anwendungsname auf der Titelleiste plaziert.</param>
+        /// <returns></returns>
+        public static string InputBox(string Prompt, string Title)
+        {
+            InputBoxSharp frm = new InputBoxSharp(Prompt, Title, "");
+            frm.ShowDialog();
+            return frm.Response(); 
+        }
+
+        /// <summary>
+        /// Zeigt eine Eingabeaufforderung in einem Dialogfeld an, wartet auf die Eingabe eines Textes oder auf das Klicken auf eine Schaltfläche durch den Benutzer und gibt eine Zeichenfolge zurück, die den Inhalt des Textfelds angibt.
+        /// </summary>
+        /// <param name="Prompt">Als Meldung im Dialogfeld angezeigter String-Ausdruck. Die maximale Länge von Prompt beträgt ungefähr 1024 Zeichen, abhängig von der Breite der verwendeten Zeichen. Wenn Prompt aus mehreren Zeilen besteht, können Sie die Zeilen trennen, indem Sie jeweils ein Wagenrücklaufzeichen(Chr(13)), ein Zeilenvorschubzeichen(Chr(10)) oder eine Kombination aus Wagenrücklauf und Zeilenvorschub (Chr(13) & Chr(10)) zwischen jeder Zeile einfügen.</param>
+        /// <param name="Title">Auf der Titelleiste des Dialogfelds angezeigter String-Ausdruck. Wenn Die Title nicht angeben, wird der Anwendungsname auf der Titelleiste plaziert.</param>
+        /// <param name="DefaultResponse">String-Ausdruck, der im Textfeld standardmäßig angezeigt wird, wenn keine andere Eingabe erfolgt. Wenn Sie DefaultResponse nicht angeben, ist das angezeigte Textfeld leer.</param> 
+        /// <returns></returns>
+        public static string InputBox(string Prompt, string Title, string DefaultResponse)
+        {
+            InputBoxSharp frm = new InputBoxSharp(Prompt, Title, DefaultResponse);
+            frm.ShowDialog();
+            return frm.Response(); 
+        }
+
+        /// <summary>
+        /// Zeigt eine Eingabeaufforderung in einem Dialogfeld an, wartet auf die Eingabe eines Textes oder auf das Klicken auf eine Schaltfläche durch den Benutzer und gibt eine Zeichenfolge zurück, die den Inhalt des Textfelds angibt.
+        /// </summary>
+        /// <param name="Prompt">Als Meldung im Dialogfeld angezeigter String-Ausdruck. Die maximale Länge von Prompt beträgt ungefähr 1024 Zeichen, abhängig von der Breite der verwendeten Zeichen. Wenn Prompt aus mehreren Zeilen besteht, können Sie die Zeilen trennen, indem Sie jeweils ein Wagenrücklaufzeichen(Chr(13)), ein Zeilenvorschubzeichen(Chr(10)) oder eine Kombination aus Wagenrücklauf und Zeilenvorschub (Chr(13) & Chr(10)) zwischen jeder Zeile einfügen.</param>
+        /// <param name="Title">Auf der Titelleiste des Dialogfelds angezeigter String-Ausdruck. Wenn Die Title nicht angeben, wird der Anwendungsname auf der Titelleiste plaziert.</param>
+        /// <param name="DefaultResponse">String-Ausdruck, der im Textfeld standardmäßig angezeigt wird, wenn keine andere Eingabe erfolgt. Wenn Sie DefaultResponse nicht angeben, ist das angezeigte Textfeld leer.</param> 
+        /// <param name="passwordchar">Angabe, ob der im Textfeld enthaltende Text durch ein Passwordchar versteckt werden soll.</param>
+        /// <returns></returns>
+        public static string InputBox(string Prompt, string Title, string DefaultResponse, bool passwordchar)
+        {
+            InputBoxSharp frm = new InputBoxSharp(Prompt, Title, DefaultResponse, passwordchar);
+            frm.ShowDialog();
+            return frm.Response();
+        }
+    }
+}
